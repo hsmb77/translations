34c86890796e4c558ca8326ee5fc083b9eee73cc
queicherius <queicherius@gmail.com>
2018-05-12 15:31:49 +0100

    Add credits.js

---
+++ credits.js
@@ -0,0 +1,23 @@
+// The entries in this file get shown on https://gw2efficiency.com/community/contributors
+// Feel free to add yourself after updating one of the translations! :)
+
+module.exports = [
+  'Ainillia.5794',
+  'Creativewild.6319',
+  'hers.7185',
+  'Index.7169',
+  'jjunior.8935',
+  'jonesarte.2980',
+  'MrSmiley.9273',
+  'Natrox.5462',
+  'Novitsh.4695',
+  'octopussy.3854',
+  'Offering of Pie.3057',
+  'razzy.8650',
+  'Saha.7218',
+  'Talas Shounika.7185',
+  'Unshatter.8210',
+  'Wiella.8567',
+  'Xerneas.4267',
+  'xPhantomNL.7230',
+].sort((a, b) => a.localeCompare(b))
